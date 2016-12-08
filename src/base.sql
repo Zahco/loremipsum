99993d724ff4755c8f617af87064bf0fb6bd4de5
Geoffrey <geoffrey.spaur@gmail.com>
2016-12-08 10:04:51 +0100

    substance / effet indésirable

--- src/base.sql
+++ src/base.sql
@@ -2,6 +2,7 @@ create type carateristique_type as object (
   description varchar(1024)
 );
 /
+create table carateristique of carateristique_type;
 
 create type patient_type as object (
   nom varchar(128),
@@ -9,11 +10,26 @@ create type patient_type as object (
   age number
 );
 /
-
-create table carateristique of carateristique_type;
 create table patient of patient_type;
 
 create table carateristique_patient (
   carateristique ref carateristique_type,
   patient ref patient_type
+);
+
+create type effet_indesirable_type (
+  description varchar(1024)
+);
+/
+create table effet_indesirable of effet_indesirable_type;
+
+create type substance_active_type (
+  description varchar(1024)
+);
+/
+create table substance_active of substance_active_type;
+
+create table effet_indesirable_substance_active (
+  effet_indesirable ref effet_indesirable_type,
+  substance_active ref substance_active_type
 );
--- src/dropforce.sql
+++ src/dropforce.sql
@@ -3,3 +3,9 @@ drop table patient force;
 drop table carateristique force;
 drop type carateristique_type force;
 drop type patient_type force;
+
+drop table effet_indesirable_substance_active force;
+drop table effet_indesirable force;
+drop table substance_active force;
+drop type effet_indesirable_type force;
+drop type substance_active_type force;
