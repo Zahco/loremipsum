010b2d18a6826e3182211eff4cc82c89a610fad6
TeeGreg <gregoire.pommier@etu.univ-rouen.fr>
2016-12-08 10:13:50 +0100

    lab + dev

--- src/base.sql
+++ src/base.sql
@@ -2,6 +2,7 @@ create type carateristique_type as object (
   description varchar(1024)
 );
 /
+create table carateristique of carateristique_type;
 
 create type patient_type as object (
   nom varchar(128),
@@ -9,11 +10,24 @@ create type patient_type as object (
   age number
 );
 /
-
-create table carateristique of carateristique_type;
 create table patient of patient_type;
 
 create table carateristique_patient (
   carateristique ref carateristique_type,
   patient ref patient_type
 );
+
+create type laboratoire_type as object (
+  nom varchar(128),
+  adresse varchar(128)
+);
+create table laboratoire of laboratoire_type;
+
+create type developpement_type as object (
+  date_debut date,
+  date_fin date,
+  medicament ref medicament_type,
+  laboratoire ref laboratoire_type
+);
+
+create table developpement of developpement_type;
