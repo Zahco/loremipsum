d3ff2ba527923bfb22645408e5ccc14686472a86
Geoffrey <geoffrey.spaur@gmail.com>
2016-12-13 09:56:21 +0100

    fix f2

--- src/fonction.sql
+++ src/fonction.sql
@@ -3,11 +3,12 @@ where deref(cp.carateristique).description = 'Femme';
 
 -- 2. une méthode donnant les traitements en cours d’un patient.
 create or replace procedure traitement_du_patient
-  (nom in patient_type.nom%type, prenom in patient_type.prenom%type)
+  (nom in patient_type.nom%type, prenom in patient_type.prenom%type, ret out traitement%rowtype)
 is
 begin
-  select deref(traitement) from traitement_prescription where
-  current_date() < deref(prescription).debut + deref(traitement).duree
+  select deref(traitement) into ret
+  from traitement_prescription
+  where current_date < deref(prescription).debut + deref(traitement).duree
   and prescription = (
     select ref(pr) from prescription pr where patient = (
       select ref(pa) from patient pa where pa.nom = nom and pa.prenom = prenom));
