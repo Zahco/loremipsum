252dfcdf94948996dbeccbcaf8fa2b494060e1c5
Unknown <geoffrey.spaur@gmail.com>
2016-12-13 12:26:25 +0100

    f3 compile but doesn't work

--- src/fonction.sql
+++ src/fonction.sql
@@ -1,5 +1,4 @@
-select deref(cp.patient) from carateristique_patient cp
-where deref(cp.carateristique).description = 'Femme';
+set serveroutput on;
 
 -- 2. une méthode donnant les traitements en cours d’un patient.
 create or replace procedure traitement_du_patient
@@ -25,6 +24,39 @@ begin
     fetch ret into line;
     exit when ret%notfound;
     dbms_output.put_line(line.nom);
+  end loop;
+  
+  close ret;
+end;
+/
+
+-- 3. Une méthode donnant la liste et le nombre d’effets indésirables connus 
+--    d’un médicament.
+
+create or replace procedure ei_par_medicament
+  (medic_nom in medicament.nom%type, ret out sys_refcursor)
+is
+begin
+  dbms_output.put_line(medic_nom);
+  open ret for 
+  select deref(es.effet_indesirable).description 
+  from effet_i_substance_a es
+  where es.substance_active = (
+    select substance_active from medicament where nom = 'doliprane'
+  );
+end;
+/
+
+declare 
+  ret sys_refcursor;
+  line effet_indesirable.description%type;
+begin
+  dbms_output.put_line('coucou');
+  ei_par_medicament('doliprane', ret);
+  loop
+    fetch ret into line;
+    exit when ret%notfound;
+    dbms_output.put_line(line);
   end loop;
   
   close ret;
